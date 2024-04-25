dd2eccdeff79fd87f600fd4e04c90b5d729085e1
sivaprakash000 <mekala.sivaprakash@gmail.com>
2024-04-25 21:51:12 +0530

    new code added

--- javascript.js
+++ javascript.js
@@ -1,20 +1,16 @@
 <!DOCTYPE html>
 <html>
-<head>
-<style>
-img {
-  position: absolute;
-  left: 0px;
-  top: 0px;
-  z-index: -1;
-}
-</style>
-</head>
 <body>
 
-<h1>This is a heading</h1>
-<img src="img_tree.png">
-<p>Because the image has a z-index of -1, it will be placed behind the text.</p>
-<p>this is the new CSS file to be added</p>
+<h2>JavaScript Numbers</h2>
+
+<p>Number can be written with or without decimals.</p>
+
+<p id="demo"></p>
+
+<script>
+document.getElementById("demo").innerHTML = 10.50;
+</script>
+
 </body>
 </html>
